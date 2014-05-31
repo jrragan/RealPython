7b74081f3617e4be72c7d7932f7044e16ee33566
rragan <rragan@cisco.com>
2014-05-30 22:20:13 -0400

    flask-hello-world dynamic route

--- flask-hello-world/app.py
+++ flask-hello-world/app.py
@@ -9,6 +9,11 @@ app = Flask(__name__)
 @app.route("/")
 @app.route("/hello")
 
+# dynamic route
+@app.route("/test/<search_query>")
+def search(search_query):
+    return search_query
+
 # define the view using a function, which returns a string
 def hello_world():
     return "Hello, World!"
